12bfbf1ccdb8124ce270af940e6dac31c299c7a3
keviny22 <kevin_young@intuit.com>
2013-03-28 20:13:50 -0700

    upped version to 0.1.1

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'kevin_young@intuit.com'
 license          'Apache 2.0'
 description      'Installs ruby'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.1.1'
 
 %w{redhat centos}.each do |os|
   supports os
