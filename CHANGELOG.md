badecb22af24d7248c7bee837f8b9322931cf4d7
capen <cbrinkley@gmail.com>
2014-03-21 12:36:16 -0700

    bumping version to 0.2.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,9 @@
 ## head
+
+## 0.2.0 (03/21/2014
 * Installs intu_ruby rpm
 * Adding Travis CI
+* Adding foodcritic and emeril
 
 ## 0.1.1 (03/29/2013):
 * Added support to disable prelink
--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'kevin_young@intuit.com'
 license          'Apache 2.0'
 description      'Installs ruby'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.1'
+version          '0.2.0'
 
 %w{redhat centos}.each do |os|
   supports os
